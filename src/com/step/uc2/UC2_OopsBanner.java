cb569ae8c72b55643b25d8d765986b7f36a4cfbb
Harshita Agrawal <your-email@example.com>
2026-03-01 22:36:04 +0530

    UC2: Implemented OOPS ASCII banner using sequential print statements

---
+++ src/com/step/uc2/UC2_OopsBanner.java
@@ -0,0 +1,16 @@
+package com.step.uc2;
+
+public class UC2_OopsBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   *****  ");
+        System.out.println("*     * *     * *           *  ");
+        System.out.println("*     * *     * *           *  ");
+        System.out.println(" *****   *****   *       ***** ");
+
+    }
+}
